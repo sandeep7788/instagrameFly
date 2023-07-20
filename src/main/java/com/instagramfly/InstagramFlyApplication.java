1ea9b4df486b248b08ff0596a055298ca319755d
sandeep <s.pareek7788@gmail.com>
2023-07-20 19:30:04 +0530

    initial commit

---
+++ src/main/java/com/instagramfly/InstagramFlyApplication.java
@@ -0,0 +1,13 @@
+package com.instagramfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instagramfly/InstagramFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagramfly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
